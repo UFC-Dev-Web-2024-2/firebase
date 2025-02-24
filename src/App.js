4336edac1a8989d4fdfd934247f3a99518096972
Lana Mesquita <lanabeatriz.mesquita@gmail.com>
2025-02-24 19:34:52 +0000

    Import firebase App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef, useEffect } from 'react';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUsrO3lFO4gOtbPwULjPOlnUx8cD26xtA",
+  authDomain: "teste-2024-2.firebaseapp.com",
+  projectId: "teste-2024-2",
+  storageBucket: "teste-2024-2.firebasestorage.app",
+  messagingSenderId: "846901344466",
+  appId: "1:846901344466:web:fa3746c5574c7fb246950b",
+  measurementId: "G-90MB5H42YT"
+};
 
 function App() {
+
+  useEffect(() => {
+    // This runs only on mount (when the component appears)
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    console.log("Oiee");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">

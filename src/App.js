78c5ac77eefdb69ee60c9396fc12cae634f18ef6
Lana Mesquita <lanabeatriz.mesquita@gmail.com>
2025-02-24 20:01:55 +0000

    Alterando tela inicial

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hospedando React no Firebase.
         </p>
         <a
           className="App-link"
